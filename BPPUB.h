66a831bae75d4be741792b424da9a3bfdb15a80c
Mathijs Barkel <54663986+MathijsBarkel@users.noreply.github.com>
2024-06-14 09:04:08 +0200

    Update BPPUB.h to contain BPP-UB*

--- BPPUB.h
+++ BPPUB.h
@@ -3,7 +3,9 @@
 
 #include "helper_functions.h"
 #include "AFCSP.h"
+#include "RM2GIFFRE.h"
 
-Solution solveBPPUB(const Instance& inst, bool stopEarly = true);
+Solution solveBPPUB(const Instance& inst, bool stopEarly = true, bool exactRecoloring = false, double timeLimit = 90);
+Solution recolorGraph(const Instance& inst, Graph& G, double timeLimit = 90);
 
 #endif
